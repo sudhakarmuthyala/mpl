c4beefe39073b6eead605e79884ffc71c250e0ae
sudhakarmuthyala <sudha7204@gmail.com>
2019-04-29 10:15:04 +0530

    Update MavenBuild.groovy

--- resources/com/griddynamics/devops/mpl/modules/Build/MavenBuild.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Build/MavenBuild.groovy
@@ -1,11 +1,16 @@
-"/**
+
+/**
+
  * Simple Maven Build
+
  */
 
-withEnv(["PATH+MAVEN=${tool(CFG.'maven.tool_version' ?: 'Maven 3')}\bin"]) {
 
-  def settings = CFG.'maven.settings_path' ? "-s '${CFG.'maven.settings_path'}'" : "
 
-  bat """mvn -B ${settings} -DargLine='-Xmx1024m -XX:MaxPermSize=1024m' clean install"""
+withEnv(["PATH+MAVEN=${tool(CFG.'maven.tool_version' ?: 'Maven 3')}/bin"]) {
+
+  def settings = CFG.'maven.settings_path' ? "-s '${CFG.'maven.settings_path'}'" : ''
+
+  sh """mvn -B ${settings} -DargLine='-Xmx1024m -XX:MaxPermSize=1024m' clean install"""
 
 }
